1f90ab8ab088f66bdbbc3073e2997959364be632
osqwety <lamborgini8032@gmail.com>
2025-01-25 22:27:18 -0600

    New Blog Post on 2025-01-25 22:27:18

--- images.py
+++ images.py
@@ -28,7 +28,7 @@ for filename in os.listdir(posts_dir):
             content = content.replace(f"[[{image}]]", markdown_image)
             
             # Step 4: Copy the image to the Hugo static/images directory if it exists
-            image_source = os.path.join(attachments_dir, image)
+            image_source = os.path.join(r"C:\Users\lambo\Documents\Obsidian_Vault\images", image)
             if image_source == "":
                 print("blank")
             print(f"yes: {image_source}")
