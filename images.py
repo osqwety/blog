3de765947eb47332d788f9226de9ab776c5ae708
osqwety <lamborgini8032@gmail.com>
2025-01-26 16:07:38 -0600

    New Blog Post on 2025-01-26 16:07:38

--- images.py
+++ images.py
@@ -17,7 +17,7 @@ for filename in os.listdir(posts_dir):
             print(content)
         
         # Step 2: Find all image links in the format ![Image Description](/images/Pasted%20image%20...%20.png)
-        images = re.findall(r'\[\[([^]]*\.(png|jpg|jpeg|gif|bmp|webp|tiff|svg))\]\]', content)
+        images = re.findall(r'\[\[([^]]+\.(?:png|jpg|jpeg|gif|bmp|webp|tiff|svg))\]\]', content)
         print(images)
         
         # Step 3: Replace image links and ensure URLs are correctly formatted
